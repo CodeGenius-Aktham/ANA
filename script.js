92bfcdffd306921a10f2de9079a742424e1be990
CodeGenius-Aktham <ghanamaktham@gmail.com>
2026-06-16 22:46:43 -0400

    link conexion

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
    CONFIG
 ───────────────────────────────────────────── */
 const CONFIG = {
-  BACKEND_URL: 'https://3f11-190-26-35-16.ngrok-free.app/api',
+  BACKEND_URL: 'https://efd0-190-26-35-16.ngrok-free.app',
 
   // Longitudes máximas por campo (seguridad de input)
   MAX_LENGTHS: {
