24e2a76ef524e924ff24ea69008475eee215ebd4
CodeGenius-Aktham <ghanamaktham@gmail.com>
2026-07-27 22:59:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const CONFIG = {
    *   DELETE BASE_URL + /empresa/inventario/propio
    *   DELETE BASE_URL + /empresa/inventario/externo
    */
-  BASE_URL: 'https://1067-186-28-189-44.ngrok-free.app',
+  BASE_URL: 'https://bc15-190-26-168-9.ngrok-free.app',
 
   MAX_LENGTHS: {
     modelName:      80,
